f521bc147a065219f8c2b811b343fd8ce6a60a21
wwm.emran <wwm.emran@gmail.com>
2024-07-29 03:05:08 +1000

    - menu edited and static links are added

--- website/urls.py
+++ website/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('index', views.index, name='index'),
+    path('home', views.index, name='index'),
     # path('', views.home, name='home'),
 ]
 
--- website/views.py
+++ website/views.py
@@ -11,6 +11,9 @@ def index(request):
 
 # def home(request):
 #     # return HttpResponse('Hello world from home')
-#     return render(request, 'home.html')
+#     # return render(request, 'index.html')
+#     return render(request, 'theme1/index.html')
+#     # return render(request, 'theme2/index.html')
+#     # return render(request, f'{request.context["theme"]}/index.html')
 
 
